75cb28d3c4e0408481a08e3fe65c02822faa5e04
JerryShen <jerry82422@gmail.com>
2016-09-24 08:21:21 +0800

    feat: Add gallery

--- main.js
+++ main.js
@@ -1,58 +1,62 @@
 $(document).ready(function(){
-	
-	
-		$('#back-top').click(function (e) {
-			e.preventDefault();
-			$('body').animate({
-				scrollTop: 0
-			}, 800);
-		});
 
-	
+  $('#back-top').click(function (e) {
+    e.preventDefault();
+    $('body').animate({
+      scrollTop: 0
+    }, 800);
+  });
 
+  $(window).scroll(function() {
 
-	$(window).scroll(function() {
     if ($(this).scrollTop()>220)
-     {
-        $('.p1').fadeOut();
-     }
+    {
+      $('.p1').fadeOut();
+    }
     else
-     {
+    {
       $('.p1').fadeIn();
-     }
-	 if ($(this).scrollTop()>220)
-     {
-        $('.p12').fadeOut();
-     }
-    else
-     {
-      $('.p12').fadeIn();
-     }
-	
-		
-	 if ($(this).scrollTop()>1700)
-     {
-        $('.appicon').fadeIn();
-     }
-    else
-     {
-      $('.appicon').fadeOut();
-     }
-		
-		
-	 if ($(this).scrollTop()>0)
-     {
-        $('.scroll').fadeOut();
-     }
-    else
-     {
-      $('.scroll').fadeIn();
-     }
-	
+    }
 
-	});
-	
-	
+    if ($(this).scrollTop()>220)
+    {
+      $('.p12').fadeOut();
+    }
+    else
+    {
+      $('.p12').fadeIn();
+    }
+
+    if ($(this).scrollTop()>1700)
+    {
+      $('.appicon').fadeIn();
+    }
+    else
+    {
+      $('.appicon').fadeOut();
+    }
+
+    if ($(this).scrollTop()>0)
+    {
+      $('.scroll').fadeOut();
+    }
+    else
+    {
+      $('.scroll').fadeIn();
+    }
+  });
+
+  new MaterialPhotoGallery(document.querySelector('.m-p-g'));
+
+  $('.gallery-btn').click(function() {
+    var $ele = $('.gallery');
+    $ele.toggleClass('show');
+  });
+
+  $('.gallery-close').click(function() {
+    $('.gallery').removeClass('show');
+  });
+
 });
 
 
