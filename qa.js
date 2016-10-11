6add97133de22b4736ac6b112eb1196aecfdfd05
JerryShen <jerry82422@gmail.com>
2016-10-11 21:34:16 +0800

    Add qa

---
+++ qa.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var $body = $('body');
+  var $qaBtn = $('.qa-btn');
+  var $qa = $('.qa');
+
+  $qa.mouseenter(function() {
+    $body.css('overflow', 'hidden');
+    $qa.css('overflow', 'auto');
+  });
+
+  $qa.mouseleave(function() {
+    $body.css('overflow', 'auto');
+    $qa.css('overflow', 'hidden');
+  });
+
+  $qaBtn.click(function() {
+    if ($qa.hasClass('show')) {
+      $qa.removeClass('show');
+      $qaBtn.removeClass('shrink');
+      $qaBtn.find('.qahint').fadeIn();
+    } else {
+      $qa.addClass('show');
+      $qaBtn.addClass('shrink');
+      $qaBtn.find('.qahint').fadeOut();
+    }
+  });
+});
